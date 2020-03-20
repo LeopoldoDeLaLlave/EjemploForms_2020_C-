9f76d2a1129ca6c43b1cf78f0ef68115c8c19b31
LeopoldoDeLaLlave <58303142+LeopoldoDeLaLlave@users.noreply.github.com>
2020-03-20 13:44:40 +0100

    Agregar archivos de proyecto.

---
+++ EjemploForms/Ventana1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace EjemploForms
+{
+    partial class Ventana1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(130, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(67, 185);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 1;
+            // 
+            // Ventana1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 450);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.label1);
+            this.Name = "Ventana1";
+            this.Text = "Ventana1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+    }
+}
